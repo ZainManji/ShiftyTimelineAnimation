dba4a85cd8c1078a146887e67e14687b6dd04a04
Zain Manji <zain@140-sfoeng50-149.clients.corp.yelpcorp.com>
2014-11-12 14:12:45 -0800

    committing all the initial set up in order to build yelp animation

---
+++ test/test.js
@@ -0,0 +1,15 @@
+var assert = chai.assert;
+
+var test = "hello";
+
+var tweenable = new Tweenable({
+  x: 50,
+  y: 100,
+  opacity: 0.5
+});
+
+describe('yelp_animation()', function () {
+    it('does something', function () {
+        assert(true);
+    });
+});
