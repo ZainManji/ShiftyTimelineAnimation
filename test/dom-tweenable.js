447bdd77c6e38f4c1a57680baa7cc076fadec8cd
Zain Manji <zainkmanji@gmail.com>
2014-11-13 16:13:27 -0800

    added more tests for dom tweenable

--- test/dom-tweenable.js
+++ test/dom-tweenable.js
@@ -2,23 +2,41 @@
 var assert = chai.assert;
 
 describe('DomTweenable', function() {
+  var stepBoolean = false;
   beforeEach(function () {
+    stepBoolean = false;
     this.config = {
       from: {opacity: 0},
       to: {opacity: 1},
       duration: 1000,
       step: function (state) {}
     };
+    this.configWithStep = {
+      from: {opacity: 0},
+      to: {opacity: 1},
+      duration: 1000,
+      step: function(state) {
+        stepBoolean = true;
+      }
+    };
     this.el = document.createElement('div');
   });
 
-  it('extend a Tweenable', function() {
+  it('constructor', function() {
     var domTweenable = new DomTweenable(this.el, this.config);
-    domTweenable.tween().pause();
-    //domTweenable.seek(250);
-    //assert.equal(testDomElement.style.opacity, 0.25);
+    assert.equal(domTweenable.tweenConfig, this.config);
   });
 
-  it('allow passing custom step function');
-  it('it update DOM node style');
+  it('allow passing custom step function', function() {
+    var domTweenable = new DomTweenable(this.el, this.configWithStep);
+    domTweenable.seek(400);
+    assert(stepBoolean, true);
+  });
+  
+  it('update DOM node style', function() {
+    var domTweenable = new DomTweenable(this.el, this.config);
+    domTweenable.seek(200);
+    assert.equal(this.el.style.opacity, 0.2);
+  });
+
 });
